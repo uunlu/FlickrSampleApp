67be24c73ab0df4b73c2a116822d967f61a54d15
Ugur Unlu <vally10@live.com>
2021-12-05 12:07:26 +0100

    implement custom error

---
+++ NetworkServices/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  NetworkServices
+//
+//  Created by Ugur Unlu on 12/5/21.
+//
+
+import Foundation
+
+public enum NetworkError: Swift.Error {
+    case invalidResponse
+    case notAuthorized
+}
