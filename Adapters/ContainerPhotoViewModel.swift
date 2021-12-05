c1ecb4200eff8893d0162012991185e8ba4b7561
Ugur Unlu <vally10@live.com>
2021-12-05 13:52:55 +0100

    move container photo view model to its file

---
+++ Adapters/ContainerPhotoViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContainerPhotoViewModel.swift
+//  Adapters
+//
+//  Created by Ugur Unlu on 12/5/21.
+//
+
+import Foundation
+
+public struct ContainerPhotoViewModel {
+    let total: Int
+    let page: Int
+    let photos: [PhotoViewModel]
+}
