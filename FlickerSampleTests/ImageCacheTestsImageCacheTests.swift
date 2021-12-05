df5f6b0ecf26f20ab617faf7a1b584789a75d459
Ugur Unlu <vally10@live.com>
2021-12-05 15:42:03 +0100

    test for cache size limit exceed

--- FlickerSampleTests/ImageCacheTestsImageCacheTests.swift
+++ FlickerSampleTests/ImageCacheTestsImageCacheTests.swift
@@ -47,6 +47,30 @@ class ImageCacheTests: XCTestCase {
         let image = sut.item(for: url)
         XCTAssertNil(image)
     }
+    
+    /// There is no need to test Apple provided frameworks and Apis
+    /// Just to demo the behaviour of NSCache, which automatically clears up the memory, when the size limit set is exceeded.
+    func test_cacheShouldHaveNilImage_when_sizeLimitBelowImageSize() {
+        let sut = ImageCache.shared
+        sut.configure(limit: 1)// 1 byte of cache size
+        let url = URL(string: "https://some-url.com/")!
+        let imageToInsert = UIImage(systemName: "lock")!
+        sut.insert(for: url, image: imageToInsert)
+        
+        let url2 = URL(string: "https://some-url2.com/")!
+        let imageToInsert2 = UIImage(systemName: "person")!
+        sut.insert(for: url2, image: imageToInsert2)
+
+        let exp = expectation(description: "wait for cache insertion")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            let image = sut.item(for: url)
+            XCTAssertNil(image)
+            exp.fulfill()
+        }
+                                      
+        wait(for: [exp], timeout: 1.1)
+    }
 }
 
 class ImageCache {
