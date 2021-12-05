cd95dea240179f0b897e7b9787b0f938486b965a
Ugur Unlu <vally10@live.com>
2021-12-05 11:31:55 +0100

    initial commit

---
+++ FlickerSample/FlickerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickerSampleApp.swift
+//  FlickerSample
+//
+//  Created by Ugur Unlu on 12/5/21.
+//
+
+import SwiftUI
+
+@main
+struct FlickerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
