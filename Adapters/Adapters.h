7fedfddd7f11ec2748e18204bd6871113ea311cc
Ugur Unlu <vally10@live.com>
2021-12-05 13:08:12 +0100

    test develop url builder

---
+++ Adapters/Adapters.h
@@ -0,0 +1,18 @@
+//
+//  Adapters.h
+//  Adapters
+//
+//  Created by Ugur Unlu on 12/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Adapters.
+FOUNDATION_EXPORT double AdaptersVersionNumber;
+
+//! Project version string for Adapters.
+FOUNDATION_EXPORT const unsigned char AdaptersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Adapters/PublicHeader.h>
+
+
